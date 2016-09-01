ea1c6350ddc68fceb7f3dbd87c7f77af2544c54d
TomasUB <rakusan2@gmail.com>
2016-09-01 12:05:32 -0400

    transferd output to docs folder

--- gulpfile.ts
+++ gulpfile.ts
@@ -33,26 +33,26 @@ let tsProject = ts.createProject(__dirname + '/tsconfig.json',tsOptions);
 
 gulp.task('html',()=>{
     gulp.src('src/pug/*.pug')
-        .pipe(changed('pages'))
+        .pipe(changed('docs'))
         .pipe(pug(pugParams))
         .on('error',onError)
-        .pipe(gulp.dest('.'))
+        .pipe(gulp.dest('docs'))
         .pipe(reload());
 });
 gulp.task('css',()=>{
     gulp.src('src/scss/*.scss')
-        .pipe(changed('pages/styles'))
+        .pipe(changed('docs/styles'))
         .pipe(sass(sassOptions))
         .on('error',onError)
         .pipe(pleeease(pleeeaseOptions))
-        .pipe(gulp.dest('styles'))
+        .pipe(gulp.dest('docs/styles'))
         .pipe(reload());
 });
 gulp.task('js',()=>{
     gulp.src(['src/ts/*.ts','typings/globals/**/*.ts', 'typings/modules/**/*.ts'])
         .pipe(ts(tsProject))
         .on('error',onError).js
-        .pipe(gulp.dest('js'))
+        .pipe(gulp.dest('docs/js'))
         .pipe(reload());
 });
 gulp.task('watch',()=>{
